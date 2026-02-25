980b9d957487534cf74d1679f739e1137e9132bd
Muyao Xiao <monetshaw@outlook.com>
2026-02-25 05:57:45 +0000

    [advec_cell_kernel]

--- src/advec_cell_kernel.f90
+++ src/advec_cell_kernel.f90
@@ -80,24 +80,19 @@ CONTAINS
 
 
     IF(dir.EQ.g_xdir) THEN
-      IF(sweep_number.EQ.1)THEN
-!$omp target teams distribute parallel do simd collapse(2)
-        DO k=y_min-2,y_max+2
-          DO j=x_min-2,x_max+2
-            pre_vol(j,k)=volume(j,k)+(vol_flux_x(j+1,k  )-vol_flux_x(j,k)+vol_flux_y(j  ,k+1)-vol_flux_y(j,k))
-            post_vol(j,k)=pre_vol(j,k)-(vol_flux_x(j+1,k  )-vol_flux_x(j,k))
-          ENDDO
-        ENDDO
-      ELSE
-!$omp target teams distribute parallel do simd collapse(2)
-        DO k=y_min-2,y_max+2
-          DO j=x_min-2,x_max+2
-            pre_vol(j,k)=volume(j,k)+vol_flux_x(j+1,k)-vol_flux_x(j,k)
-            post_vol(j,k)=volume(j,k)
-          ENDDO
-        ENDDO
 
+      IF(sweep_number.EQ.1)THEN
+        !$omp target teams workdistribute
+          pre_vol(x_min-2:x_max+2,y_min-2:y_max+2)=volume(x_min-2:x_max+2,y_min-2:y_max+2)+(vol_flux_x(x_min-1:x_max+3,y_min-2:y_max+2)-vol_flux_x(x_min-2:x_max+2,y_min-2:y_max+2)+vol_flux_y(x_min-2:x_max+2  ,y_min-1:y_max+3)-vol_flux_y(x_min-2:x_max+2,y_min-2:y_max+2))
+          post_vol(x_min-2:x_max+2,y_min-2:y_max+2)=pre_vol(x_min-2:x_max+2,y_min-2:y_max+2)-(vol_flux_x(x_min-1:x_max+3,y_min-2:y_max+2  )-vol_flux_x(x_min-2:x_max+2,y_min-2:y_max+2))
+        !$omp end target teams workdistribute
+      ELSE
+        !$omp target teams workdistribute
+          pre_vol(x_min-2:x_max+2,y_min-2:y_max+2)=volume(x_min-2:x_max+2,y_min-2:y_max+2)+vol_flux_x(x_min-1:x_max+3,y_min-2:y_max+2)-vol_flux_x(x_min-2:x_max+2,y_min-2:y_max+2)
+          post_vol(x_min-2:x_max+2,y_min-2:y_max+2)=volume(x_min-2:x_max+2,y_min-2:y_max+2)
+        !$omp end target teams workdistribute
       ENDIF
+
 !$omp target teams distribute parallel do simd collapse(2)
       DO k=y_min,y_max
         DO j=x_min,x_max+2
@@ -164,22 +159,17 @@ CONTAINS
     ELSEIF(dir.EQ.g_ydir) THEN
 
       IF(sweep_number.EQ.1)THEN
-!$omp target teams distribute parallel do simd collapse(2)
-        DO k=y_min-2,y_max+2
-          DO j=x_min-2,x_max+2
-            pre_vol(j,k)=volume(j,k)+(vol_flux_y(j  ,k+1)-vol_flux_y(j,k)+vol_flux_x(j+1,k  )-vol_flux_x(j,k))
-            post_vol(j,k)=pre_vol(j,k)-(vol_flux_y(j  ,k+1)-vol_flux_y(j,k))
-          ENDDO
-        ENDDO
+        !$omp target teams workdistribute
+          pre_vol(x_min-2:x_max+2,y_min-2:y_max+2)=volume(x_min-2:x_max+2,y_min-2:y_max+2)+(vol_flux_y(x_min-2:x_max+2,y_min-1:y_max+3)-vol_flux_y(x_min-2:x_max+2,y_min-2:y_max+2)+vol_flux_x(x_min-1:x_max+3,y_min-2:y_max+2  )-vol_flux_x(x_min-2:x_max+2,y_min-2:y_max+2))
+          post_vol(x_min-2:x_max+2,y_min-2:y_max+2)=pre_vol(x_min-2:x_max+2,y_min-2:y_max+2)-(vol_flux_y(x_min-2:x_max+2,y_min-1:y_max+3)-vol_flux_y(x_min-2:x_max+2,y_min-2:y_max+2))
+        !$omp end target teams workdistribute
       ELSE
-!$omp target teams distribute parallel do simd collapse(2)
-        DO k=y_min-2,y_max+2
-          DO j=x_min-2,x_max+2
-            pre_vol(j,k)=volume(j,k)+vol_flux_y(j  ,k+1)-vol_flux_y(j,k)
-            post_vol(j,k)=volume(j,k)
-          ENDDO
-        ENDDO
+        !$omp target teams workdistribute
+          pre_vol(x_min-2:x_max+2,y_min-2:y_max+2)=volume(x_min-2:x_max+2,y_min-2:y_max+2)+vol_flux_y(x_min-2:x_max+2,y_min-1:y_max+3)-vol_flux_y(x_min-2:x_max+2,y_min-2:y_max+2)
+          post_vol(x_min-2:x_max+2,y_min-2:y_max+2)=volume(x_min-2:x_max+2,y_min-2:y_max+2)
+        !$omp end target teams workdistribute
       ENDIF
+
 !$omp target teams distribute parallel do simd collapse(2)
       DO k=y_min,y_max+2
         DO j=x_min,x_max
