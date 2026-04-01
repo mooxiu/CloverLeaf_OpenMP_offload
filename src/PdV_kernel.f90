bc0da95c93efbcbb9d7117ac80aacbd7dba8aa92
Muyao Xiao <monetshaw@outlook.com>
2026-04-01 02:27:38 +0000

    test after implementing private

--- src/PdV_kernel.f90
+++ src/PdV_kernel.f90
@@ -64,72 +64,129 @@ CONTAINS
     REAL(KIND=8)  :: right_flux,left_flux,top_flux,bottom_flux,total_flux
     REAL(KIND=8)  :: volume_change_s
 
+    REAL(KIND=8), DIMENSION(x_min-2:x_max+3,y_min-2:y_max+3) :: right_flux_arr,left_flux_arr,top_flux_arr,bottom_flux_arr,total_flux_arr,min_cell_volume_arr,energy_change_arr,recip_volume_arr,volume_change_s_arr
+
     IF(predict)THEN
-!$omp target teams distribute parallel do simd collapse(2)
-!PRIVATE(right_flux,left_flux,top_flux,bottom_flux,total_flux,min_cell_volume,energy_change,recip_volume,volume_change_s)
-      DO k=y_min,y_max
-        DO j=x_min,x_max
+! !$omp target teams distribute parallel do simd collapse(2)
+! !PRIVATE(right_flux,left_flux,top_flux,bottom_flux,total_flux,min_cell_volume,energy_change,recip_volume,volume_change_s)
+!       DO k=y_min,y_max
+!         DO j=x_min,x_max
+!
+!           left_flux=  (xarea(j  ,k  )*(xvel0(j  ,k  )+xvel0(j  ,k+1)                     &
+!             +xvel0(j  ,k  )+xvel0(j  ,k+1)))*0.25_8*dt*0.5
+!           right_flux= (xarea(j+1,k  )*(xvel0(j+1,k  )+xvel0(j+1,k+1)                     &
+!             +xvel0(j+1,k  )+xvel0(j+1,k+1)))*0.25_8*dt*0.5
+!           bottom_flux=(yarea(j  ,k  )*(yvel0(j  ,k  )+yvel0(j+1,k  )                     &
+!             +yvel0(j  ,k  )+yvel0(j+1,k  )))*0.25_8*dt*0.5
+!           top_flux=   (yarea(j  ,k+1)*(yvel0(j  ,k+1)+yvel0(j+1,k+1)                     &
+!             +yvel0(j  ,k+1)+yvel0(j+1,k+1)))*0.25_8*dt*0.5
+!           total_flux=right_flux-left_flux+top_flux-bottom_flux
+!
+!           volume_change_s=volume(j,k)/(volume(j,k)+total_flux)
+!
+!           min_cell_volume=MIN(volume(j,k)+right_flux-left_flux+top_flux-bottom_flux &
+!             ,volume(j,k)+right_flux-left_flux                      &
+!             ,volume(j,k)+top_flux-bottom_flux)
+!
+!           recip_volume=1.0/volume(j,k)
+!
+!           energy_change=(pressure(j,k)/density0(j,k)+viscosity(j,k)/density0(j,k))*total_flux*recip_volume
+!
+!           energy1(j,k)=energy0(j,k)-energy_change
+!
+!           density1(j,k)=density0(j,k)*volume_change_s
+!
+!         ENDDO
+!       ENDDO
 
-          left_flux=  (xarea(j  ,k  )*(xvel0(j  ,k  )+xvel0(j  ,k+1)                     &
-            +xvel0(j  ,k  )+xvel0(j  ,k+1)))*0.25_8*dt*0.5
-          right_flux= (xarea(j+1,k  )*(xvel0(j+1,k  )+xvel0(j+1,k+1)                     &
-            +xvel0(j+1,k  )+xvel0(j+1,k+1)))*0.25_8*dt*0.5
-          bottom_flux=(yarea(j  ,k  )*(yvel0(j  ,k  )+yvel0(j+1,k  )                     &
-            +yvel0(j  ,k  )+yvel0(j+1,k  )))*0.25_8*dt*0.5
-          top_flux=   (yarea(j  ,k+1)*(yvel0(j  ,k+1)+yvel0(j+1,k+1)                     &
-            +yvel0(j  ,k+1)+yvel0(j+1,k+1)))*0.25_8*dt*0.5
-          total_flux=right_flux-left_flux+top_flux-bottom_flux
+!$omp target teams workdistribute private(right_flux_arr,left_flux_arr,top_flux_arr,bottom_flux_arr,total_flux_arr,min_cell_volume_arr,energy_change_arr,recip_volume_arr,volume_change_s_arr)
+          left_flux_arr(x_min:x_max,y_min:y_max)=  (xarea(x_min:x_max  ,y_min:y_max  )*(xvel0(x_min:x_max  ,y_min:y_max  )+xvel0(x_min:x_max  ,y_min+1:y_max+1)                     &
+            +xvel0(x_min:x_max  ,y_min:y_max  )+xvel0(x_min:x_max  ,y_min+1:y_max+1)))*0.25_8*dt*0.5
+          right_flux_arr(x_min:x_max,y_min:y_max)= (xarea(x_min+1:x_max+1,y_min:y_max  )*(xvel0(x_min+1:x_max+1,y_min:y_max  )+xvel0(x_min+1:x_max+1,y_min+1:y_max+1)                     &
+            +xvel0(x_min+1:x_max+1,y_min:y_max  )+xvel0(x_min+1:x_max+1,y_min+1:y_max+1)))*0.25_8*dt*0.5
+          bottom_flux_arr(x_min:x_max,y_min:y_max)=(yarea(x_min:x_max  ,y_min:y_max  )*(yvel0(x_min:x_max  ,y_min:y_max  )+yvel0(x_min+1:x_max+1,y_min:y_max  )                     &
+            +yvel0(x_min:x_max  ,y_min:y_max  )+yvel0(x_min+1:x_max+1,y_min:y_max  )))*0.25_8*dt*0.5
+          top_flux_arr(x_min:x_max,y_min:y_max)=   (yarea(x_min:x_max  ,y_min+1:y_max+1)*(yvel0(x_min:x_max  ,y_min+1:y_max+1)+yvel0(x_min+1:x_max+1,y_min+1:y_max+1)                     &
+            +yvel0(x_min:x_max  ,y_min+1:y_max+1)+yvel0(x_min+1:x_max+1,y_min+1:y_max+1)))*0.25_8*dt*0.5
+          total_flux_arr(x_min:x_max,y_min:y_max)=right_flux_arr(x_min:x_max,y_min:y_max)-left_flux_arr(x_min:x_max,y_min:y_max)+top_flux_arr(x_min:x_max,y_min:y_max)-bottom_flux_arr(x_min:x_max,y_min:y_max)
 
-          volume_change_s=volume(j,k)/(volume(j,k)+total_flux)
+          volume_change_s_arr(x_min:x_max,y_min:y_max)=volume(x_min:x_max,y_min:y_max)/(volume(x_min:x_max,y_min:y_max)+total_flux_arr(x_min:x_max,y_min:y_max))
 
-          min_cell_volume=MIN(volume(j,k)+right_flux-left_flux+top_flux-bottom_flux &
-            ,volume(j,k)+right_flux-left_flux                      &
-            ,volume(j,k)+top_flux-bottom_flux)
+          min_cell_volume_arr(x_min:x_max,y_min:y_max)=MIN(volume(x_min:x_max,y_min:y_max)+right_flux_arr(x_min:x_max,y_min:y_max)-left_flux_arr(x_min:x_max,y_min:y_max)+top_flux_arr(x_min:x_max,y_min:y_max)-bottom_flux_arr(x_min:x_max,y_min:y_max) &
+            ,volume(x_min:x_max,y_min:y_max)+right_flux_arr(x_min:x_max,y_min:y_max)-left_flux_arr(x_min:x_max,y_min:y_max)                      &
+            ,volume(x_min:x_max,y_min:y_max)+top_flux_arr(x_min:x_max,y_min:y_max)-bottom_flux_arr(x_min:x_max,y_min:y_max))
  
-          recip_volume=1.0/volume(j,k)
+          recip_volume_arr(x_min:x_max,y_min:y_max)=1.0/volume(x_min:x_max,y_min:y_max)
 
-          energy_change=(pressure(j,k)/density0(j,k)+viscosity(j,k)/density0(j,k))*total_flux*recip_volume
+          energy_change_arr(x_min:x_max,y_min:y_max)=(pressure(x_min:x_max,y_min:y_max)/density0(x_min:x_max,y_min:y_max)+viscosity(x_min:x_max,y_min:y_max)/density0(x_min:x_max,y_min:y_max))*total_flux_arr(x_min:x_max,y_min:y_max)*recip_volume_arr(x_min:x_max,y_min:y_max)
 
-          energy1(j,k)=energy0(j,k)-energy_change
+          energy1(x_min:x_max,y_min:y_max)=energy0(x_min:x_max,y_min:y_max)-energy_change_arr(x_min:x_max,y_min:y_max)
 
-          density1(j,k)=density0(j,k)*volume_change_s
+          density1(x_min:x_max,y_min:y_max)=density0(x_min:x_max,y_min:y_max)*volume_change_s_arr(x_min:x_max,y_min:y_max)
 
-        ENDDO
-      ENDDO
+!$omp end target teams workdistribute 
 
 
     ELSE
-!$omp target teams distribute parallel do simd collapse(2)
-      DO k=y_min,y_max
-!PRIVATE(right_flux,left_flux,top_flux,bottom_flux,total_flux,min_cell_volume,energy_change,recip_volume,volume_change_s)
-        DO j=x_min,x_max
 
-          left_flux=  (xarea(j  ,k  )*(xvel0(j  ,k  )+xvel0(j  ,k+1)                     &
-            +xvel1(j  ,k  )+xvel1(j  ,k+1)))*0.25_8*dt
-          right_flux= (xarea(j+1,k  )*(xvel0(j+1,k  )+xvel0(j+1,k+1)                     &
-            +xvel1(j+1,k  )+xvel1(j+1,k+1)))*0.25_8*dt
-          bottom_flux=(yarea(j  ,k  )*(yvel0(j  ,k  )+yvel0(j+1,k  )                     &
-            +yvel1(j  ,k  )+yvel1(j+1,k  )))*0.25_8*dt
-          top_flux=   (yarea(j  ,k+1)*(yvel0(j  ,k+1)+yvel0(j+1,k+1)                     &
-            +yvel1(j  ,k+1)+yvel1(j+1,k+1)))*0.25_8*dt
-          total_flux=right_flux-left_flux+top_flux-bottom_flux
+! !$omp target teams distribute parallel do simd collapse(2)
+!       DO k=y_min,y_max
+! !PRIVATE(right_flux,left_flux,top_flux,bottom_flux,total_flux,min_cell_volume,energy_change,recip_volume,volume_change_s)
+!         DO j=x_min,x_max
+!
+!           left_flux=  (xarea(j  ,k  )*(xvel0(j  ,k  )+xvel0(j  ,k+1)                     &
+!             +xvel1(j  ,k  )+xvel1(j  ,k+1)))*0.25_8*dt
+!           right_flux= (xarea(j+1,k  )*(xvel0(j+1,k  )+xvel0(j+1,k+1)                     &
+!             +xvel1(j+1,k  )+xvel1(j+1,k+1)))*0.25_8*dt
+!           bottom_flux=(yarea(j  ,k  )*(yvel0(j  ,k  )+yvel0(j+1,k  )                     &
+!             +yvel1(j  ,k  )+yvel1(j+1,k  )))*0.25_8*dt
+!           top_flux=   (yarea(j  ,k+1)*(yvel0(j  ,k+1)+yvel0(j+1,k+1)                     &
+!             +yvel1(j  ,k+1)+yvel1(j+1,k+1)))*0.25_8*dt
+!           total_flux=right_flux-left_flux+top_flux-bottom_flux
+!
+!           volume_change_s=volume(j,k)/(volume(j,k)+total_flux)
+!
+!           min_cell_volume=MIN(volume(j,k)+right_flux-left_flux+top_flux-bottom_flux &
+!             ,volume(j,k)+right_flux-left_flux                      &
+!             ,volume(j,k)+top_flux-bottom_flux)
+!
+!           recip_volume=1.0/volume(j,k)
+!
+!           energy_change=(pressure(j,k)/density0(j,k)+viscosity(j,k)/density0(j,k))*total_flux*recip_volume
+!
+!           energy1(j,k)=energy0(j,k)-energy_change
+!
+!           density1(j,k)=density0(j,k)*volume_change_s
+!
+!         ENDDO
+!       ENDDO
 
-          volume_change_s=volume(j,k)/(volume(j,k)+total_flux)
 
-          min_cell_volume=MIN(volume(j,k)+right_flux-left_flux+top_flux-bottom_flux &
-            ,volume(j,k)+right_flux-left_flux                      &
-            ,volume(j,k)+top_flux-bottom_flux)
+!$omp target teams workdistribute private(right_flux_arr,left_flux_arr,top_flux_arr,bottom_flux_arr,total_flux_arr,min_cell_volume_arr,energy_change_arr,recip_volume_arr,volume_change_s_arr)
+          left_flux_arr(x_min:x_max,y_min:y_max)=  (xarea(x_min:x_max  ,y_min:y_max  )*(xvel0(x_min:x_max  ,y_min:y_max)+xvel0(x_min:x_max  ,y_min+1:y_max+1)                     &
+            +xvel1(x_min:x_max  ,y_min:y_max  )+xvel1(x_min:x_max  ,y_min+1:y_max+1)))*0.25_8*dt
+          right_flux_arr(x_min:x_max,y_min:y_max)= (xarea(x_min+1:x_max+1,y_min:y_max  )*(xvel0(x_min+1:x_max+1,y_min:y_max)+xvel0(x_min+1:x_max+1,y_min+1:y_max+1)                     &
+            +xvel1(x_min+1:x_max+1,y_min:y_max  )+xvel1(x_min+1:x_max+1,y_min+1:y_max+1)))*0.25_8*dt
+          bottom_flux_arr(x_min:x_max,y_min:y_max)=(yarea(x_min:x_max  ,y_min:y_max  )*(yvel0(x_min:x_max  ,y_min:y_max)+yvel0(x_min+1:x_max+1,y_min:y_max  )                     &
+            +yvel1(x_min:x_max  ,y_min:y_max  )+yvel1(x_min+1:x_max+1,y_min:y_max  )))*0.25_8*dt
+          top_flux_arr(x_min:x_max,y_min:y_max)=   (yarea(x_min:x_max  ,y_min+1:y_max+1)*(yvel0(x_min:x_max,y_min+1:y_max+1)+yvel0(x_min+1:x_max+1,y_min+1:y_max+1)                     &
+            +yvel1(x_min:x_max  ,y_min+1:y_max+1)+yvel1(x_min+1:x_max+1,y_min+1:y_max+1)))*0.25_8*dt
+          total_flux_arr(x_min:x_max,y_min:y_max)=right_flux_arr(x_min:x_max,y_min:y_max)-left_flux_arr(x_min:x_max,y_min:y_max)+top_flux_arr(x_min:x_max,y_min:y_max)-bottom_flux_arr(x_min:x_max,y_min:y_max)
+
+          volume_change_s_arr(x_min:x_max,y_min:y_max)=volume(x_min:x_max,y_min:y_max)/(volume(x_min:x_max,y_min:y_max)+total_flux_arr(x_min:x_max,y_min:y_max))
+
+          min_cell_volume_arr(x_min:x_max,y_min:y_max)=MIN(volume(x_min:x_max,y_min:y_max)+right_flux_arr(x_min:x_max,y_min:y_max)-left_flux_arr(x_min:x_max,y_min:y_max)+top_flux_arr(x_min:x_max,y_min:y_max)-bottom_flux_arr(x_min:x_max,y_min:y_max) &
+            ,volume(x_min:x_max,y_min:y_max)+right_flux_arr(x_min:x_max,y_min:y_max)-left_flux_arr(x_min:x_max,y_min:y_max)                      &
+            ,volume(x_min:x_max,y_min:y_max)+top_flux_arr(x_min:x_max,y_min:y_max)-bottom_flux_arr(x_min:x_max,y_min:y_max))
  
-          recip_volume=1.0/volume(j,k)
+          recip_volume_arr(x_min:x_max,y_min:y_max)=1.0/volume(x_min:x_max,y_min:y_max)
 
-          energy_change=(pressure(j,k)/density0(j,k)+viscosity(j,k)/density0(j,k))*total_flux*recip_volume
+          energy_change_arr(x_min:x_max,y_min:y_max)=(pressure(x_min:x_max,y_min:y_max)/density0(x_min:x_max,y_min:y_max)+viscosity(x_min:x_max,y_min:y_max)/density0(x_min:x_max,y_min:y_max))*total_flux_arr(x_min:x_max,y_min:y_max)*recip_volume_arr(x_min:x_max,y_min:y_max)
 
-          energy1(j,k)=energy0(j,k)-energy_change
+          energy1(x_min:x_max,y_min:y_max)=energy0(x_min:x_max,y_min:y_max)-energy_change_arr(x_min:x_max,y_min:y_max)
 
-          density1(j,k)=density0(j,k)*volume_change_s
-
-        ENDDO
-      ENDDO
+          density1(x_min:x_max,y_min:y_max)=density0(x_min:x_max,y_min:y_max)*volume_change_s_arr(x_min:x_max,y_min:y_max)
+!$omp end target teams workdistribute
 
   ENDIF
 
